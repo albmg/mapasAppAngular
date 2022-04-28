4c48b9c396e64c989bc92a2026b8cf140b10462f
albmg <albmguer@gmail.com>
2022-04-28 01:47:31 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxibWciLCJhIjoiY2t6cjYwbjYzMDdiMDJydDk1azdjZzh6cCJ9.15-9RFqsk3d24jJqv_Xg1w';
+
+if (!navigator.geolocation) {
+  alert(' Navegador no soporta geolocalización ')
+  throw new Error(' Navegador no soporta geolocalización ')
+}
+
 if (environment.production) {
   enableProdMode();
 }
